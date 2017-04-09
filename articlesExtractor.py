c9d88ea37d082422be54c5556a4f0e779b472a68
adeelahuma <ahuma@vt.edu>
2017-04-09 19:31:01 -0400

    Update articlesExtractor.py

--- articlesExtractor.py
+++ articlesExtractor.py
@@ -1,23 +1,38 @@
 import pickle
 
-def main():
-	file_name = "/Users/adeelahuma/Documents/Grad School/Data Analytics 2/data/fake_news.pickle"
-	fake_news_dir = "/Users/adeelahuma/Documents/Grad School/Data Analytics 2/data/fake_news/"
-	
+def extractFiles(file_name, news_dir, article_type):
 	file_object = open(file_name, 'r')
 	
 	files = pickle.load(file_object)
 	
-	print 'copying article files...'
+	print 'copying ' + article_type + ' article files...'
+	print len(files)
 	
 	i=0
 	for file in files:
-		ff_name= fake_news_dir+"fake_" + str(i) + ".txt"
+		print 
+		ff_name = news_dir + article_type + "_" + str(i) + ".txt"
 		print ff_name
-		fake_file = open(ff_name, "w")
-		fake_file.write(str(file))
-		fake_file.close()
+		news_file = open(ff_name, "w")
+		content = (file['content']).encode('utf-8')
+
+		news_file.write(content)  ##save only file's content
+		news_file.close()
 		i = i + 1
+	
+
+def main():
+	
+	file_name= "./data20170326/fake_news.pickle"
+	news_dir = "./data20170326/fake_news/"
+	
+	extractFiles(file_name, news_dir, "fake")
+	
+	file_name = "./data20170326/real_news.pickle"
+	news_dir = "./data20170326/real_news/"
+	
+	extractFiles(file_name, news_dir, "real")
+		
 		
 if __name__ == "__main__":
 	main()
